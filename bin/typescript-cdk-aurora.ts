d84da04a5bd285595058074a391b2d63c70fb0a7
Erich <erich.schuetz@yahoo.com>
2026-03-06 11:45:12 -0600

    Added the env to bin

--- bin/typescript-cdk-aurora.ts
+++ bin/typescript-cdk-aurora.ts
@@ -4,6 +4,8 @@ import { TypescriptCdkAuroraStack } from '../lib/typescript-cdk-aurora-stack';
 
 const app = new cdk.App();
 new TypescriptCdkAuroraStack(app, 'TypescriptCdkAuroraStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
